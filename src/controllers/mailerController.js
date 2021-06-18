b5ec3fca8680fdd8005978b9d559d253a50041c6
Jorge Fernandez <fsjorgeluis@gmail.com>
2021-06-17 22:53:31 -0400

    Added some changes to mailer service, now using sendgrid instead of nodemailer

--- src/controllers/mailerController.js
+++ src/controllers/mailerController.js
@@ -1,43 +1,51 @@
+import sgMail from '@sendgrid/mail';
 import asyncHandler from 'express-async-handler';
-import transporter from '../config/smtpTransport.js';
 
-const sendMail = asyncHandler(async (req, res) => {
+
+export const sendMail = asyncHandler(async (req, res) => {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+    const { name, email, services, budget, message } = req.body;
+
+    if (!name || typeof name !== 'string') {
+        throw new Error('Name was not defined');
+    }
+    if (!email || typeof email !== 'string') {
+        throw new Error('Email was not defined');
+    }
+    if (!message || typeof message !== 'string') {
+        throw new Error('Message was not defined');
+    }
+
+    const msg = {
+        to: process.env.EMAIL_TO, // Your recipient
+        from: process.env.FROM_EMAIL, // Verified Sendgrid sender
+        subject: 'Email sent with SendGrid from portfolio',
+        text: `Hey, i'm ${name}, this is my email: ${email}, 
+            and wanna leave you a message: ${message}.
+            If i decide to hire you, this are some services that i need an the budget to make it done
+            Services: ${services}.
+            Budget: ${budget}`,
+        html: `<div>
+            Hey, i'm <strong>${name}</strong>,<br>
+            this is my email: <strong>${email}</strong>,<br> 
+            and wanna leave you a message: <i>${message}</i><br>
+            If i decide to hire you, this are some services that i need an the budget to make it done <br>
+            <strong>Services:</strong> <i>${services}</i>.<br>
+            <strong>Budget:</strong> <i>${budget}</i>
+            </div>`,
+    };
+
     try {
-        const { fromName, toEmail, subject, textBody, htmlBody } = req.body;
-
-        if (!subject || typeof subject !== 'string') {
-            throw new Error('Subject was not defined');
-        }
-        if (!toEmail || typeof toEmail !== 'string') {
-            throw new Error('To Email was not defined');
-        }
-
-        const mailOptions = {
-            from: fromName + ' <' + process.env.FROM_EMAIL + '>',
-            to: toEmail,
-            subject: subject,
-            text: textBody,
-            html: htmlBody
-        };
-        // console.log(mailOptions)
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                throw new Error(error.message);
-            }
-            console.log('Message %s sent: %s', info.messageId, info.response);
-            res.json({
-                message: 'Message sent successfully!'
-            })
-        })
-    } catch (error) {
-        res.status(500)
+        await sgMail.send(msg)
         res.json({
+            success: 'ok',
+            message: 'Email sent'
+        });
+    } catch (error) {
+        res.status(500).json({
             message: 'Something is wrong:',
             error: error.message
-        })
-        console.error('Something is wrong:')
-        console.error(error.message)
+        });
     }
 });
-
-export { sendMail };
